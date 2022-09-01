e7af4ef813a308c0b93564b9e3da0f07051f3e19
mer404 <tushalmer186@gmial.com>
2022-09-01 18:59:12 +0530

    initial commit

---
+++ app/src/main/java/com/example/expensemanager/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.expensemanager.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.expensemanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+
+    }
+
+    private fun initView() {
+
+    }
+}
